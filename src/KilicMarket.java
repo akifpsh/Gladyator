19b810478c4420249a25f097e264c5eda8bc8d73
Akif <akif.egem@hotmail.com>
2022-12-16 15:21:32 +0300

    Update KilicMarket.java

--- src/KilicMarket.java
+++ src/KilicMarket.java
@@ -1,25 +1,51 @@
+import java.util.Scanner;
+
 public class KilicMarket extends Market{
-   //private Kahraman kahraman;
-   /*public KilicMarket(Kahraman kahraman){
-        this.kahraman = kahraman;
-        WelcomeMenu();
-   }*/
+   public KilicMarket(Kahraman kahraman) {
+        super(kahraman);
+    }
+private Kahraman kahraman;
 
 
+   Scanner sc=new Scanner(System.in);
+
+    private int secim;
+    
     @Override
     public void WelcomeMenu(){
         System.out.println("KilicMarkete Hos Geldiniz");
         System.out.println("Ne Satın Almak Istersiniz ?");
         System.out.println("1-->Taş kılıç   ATK: 130");
-        System.out.println("2-->Demir kılıç");
-        System.out.println("3-->Altın kılıç");
-        System.out.println("4-->Elmas kılıç");
-        
-    }
+        System.out.println("2-->Demir kılıç ATK: 250");
+        System.out.println("3-->Altın kılıç ATK: 400");
+        System.out.println("4-->Elmas kılıç ATK: 750");
+        System.out.println("5-->Markete Geri Don");
 
-    
-    
-    
+        this.secim=sc.nextInt();
+        switch(secim){
+            case 1:
+                kahraman.setKilicIsmi("Tas");
+                WelcomeMenu();
+                break;
+            case 2:
+                kahraman.setKilicIsmi("Demir");
+                WelcomeMenu();
+                break;
+            case 3:
+                kahraman.setKilicIsmi("Altin");
+                WelcomeMenu();
+                break;
+            case 4:
+                kahraman.setKilicIsmi("Elmas");
+                WelcomeMenu();
+                break;
+            case 5:
+                break;
+            default:
+                WelcomeMenu();
+        }
+
+    }
 }
 
 
